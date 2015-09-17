9f256036afb786c8bb15d809580e833f126476e9
CodeSun <cscodesun@gmail.com>
2015-09-17 18:42:09 +0800

    [dev]add handler

---
+++ src/handler.h
@@ -0,0 +1,22 @@
+#ifndef _HANDLER_H_
+#define _HAHDLER_H_
+
+#include <unistd.h>
+
+typedef int fd_t;
+
+typedef enum htype {
+	H_NONE,
+	H_FILE,
+	H_SOCK,
+	H_POLL
+} htype_t;
+
+typedef struct handler {
+	fd_t    fileno;
+	htype_t type;
+} *handler_t;
+
+#define handler_close(h) close(h)
+
+#endif // _HANDLER_H_
