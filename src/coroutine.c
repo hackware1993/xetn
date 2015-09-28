88731ee55089b578a8b1540f607acaf3ed90b3f1
CodeSun <cscodesun@gmail.com>
2015-09-28 11:37:35 +0800

    [dev]add state for coroutine & remove jmp_buf

--- src/coroutine.c
+++ src/coroutine.c
@@ -1,34 +1,105 @@
 #include "coroutine.h"
 
-static void** __keys() {
+#include <stdio.h>
+
+#define PRIVATE static
+
+PRIVATE void __bridge() {
+	/* get the pointer to the keys */
 	void* zone;
 	zone = (void*)(((long)&zone + PAGE_SIZE - 1) / PAGE_SIZE * PAGE_SIZE);
 	zone += -sizeof(void*) * 12;
-	return (void**)zone;
-}
+	void** keys = (void**)zone;
+	Coroutine coro = (Coroutine)keys[0];
 
-static void __bridge() {
-	coroutine_t coro = (coroutine_t)__keys()[0];
+MAIN_RUN:
 	coro->main(coro);
+END_AGAIN:
+	coro->state = C_END;
+	switch(setreg(coro->env)) {
+		case 0:
+			regjmp(coro->ctx, 1);
+	/* NOTICE: pc NEVER point to this position */
+		case -1:
+			coroutine_yield(coro);
+			goto MAIN_RUN;
+		default:
+	/* can not resume coroutine whose state is C_END */
+	/* this situation can be seen as a kind of error */
+			goto END_AGAIN;
+	}
+	coroutine_yield(coro);
+	/* NOTICE: pc NEVER point to this position */
 }
 
-coroutine_t coroutine_new() {
+Coroutine coroutine_new() {
 	void* stk = aligned_alloc(PAGE_SIZE, STK_DEFAULT_SIZE);
-	coroutine_t coro = (coroutine_t)stk;
+	Coroutine coro = (Coroutine)stk;
+	coro->state = C_INIT;
 	coro->stk = stk;
 	coro->bot = stk + GREEN_ZONE;
 	coro->top = stk + (STK_DEFAULT_SIZE - RED_ZONE);
 	return coro;
 }
 
-void coroutine_init(coroutine_t coro, coro_cb_t main) {
-	if(setreg(coro->buf)) {
+void coroutine_init(Coroutine coro, regbuf_t ctx, coro_cb_t main) {
+	if(setreg(coro->env)) {
 		__bridge();
+		/* NOTICE: pc NEVER point to this position */
 	}
+	coro->state = C_PEND;
+	coro->ctx = ctx;
 	coro->main = main;
-	coro->sp = (void*)coro->buf->__jmpbuf[0];
-	coro->buf->__jmpbuf[0] = (long)coro->top;
+	coro->sp = coro->env[0];
+	coro->env[0] = coro->top;
 	void* keys = coro->stk + (STK_DEFAULT_SIZE - sizeof(void*) * 12);
 	((void**)keys)[0] = coro;
 }
 
+/* the following functions is used for DEBUG */
+void coroutine_dump_regs(Coroutine coro) {
+#if defined(__i386__)
+	printf("[\e[1;32mSP\e[0m] %08X\n", coro->env[0]);
+	printf("[\e[1;32mBX\e[0m] %08X\n", coro->env[1]);
+	printf("[\e[1;32mSI\e[0m] %08X\n", coro->env[2]);
+	printf("[\e[1;32mDI\e[0m] %08X\n", coro->env[3]);
+	printf("[\e[1;32mBP\e[0m] %08X\n", coro->env[4]);
+	printf("[\e[1;32mPC\e[0m] %08X\n", coro->env[5]);
+#elif defined(__amd64__) || defined(__x86_64__)
+	printf("[\e[1;32mRSP\e[0m] %016lx\n", coro->env[0]);
+	printf("[\e[1;32mRBX\e[0m] %016lx\n", coro->env[1]);
+	printf("[\e[1;32mRBP\e[0m] %016lx\n", coro->env[2]);
+	printf("[\e[1;32mR12\e[0m] %016lx\n", coro->env[3]);
+	printf("[\e[1;32mR13\e[0m] %016lx\n", coro->env[4]);
+	printf("[\e[1;32mR14\e[0m] %016lx\n", coro->env[5]);
+	printf("[\e[1;32mR15\e[0m] %016lx\n", coro->env[6]);
+	printf("[\e[1;32mRPC\e[0m] %016lx\n", coro->env[7]);
+#endif
+}
+
+#if defined(__i386__)
+
+#define PLEN 4
+
+#elif defined(__amd64__) || defined(__x86_64__)
+
+#define PLEN 8
+
+#endif
+
+void coroutine_dump_stack(Coroutine coro) {
+	void* p = coro->top + (-PLEN * PLEN);
+	int i;
+	while(p >= coro->bot) {
+		printf("[\e[1;32m%X\e[0m]", p);
+		for(i = 0; i < PLEN; ++i) {
+		#if defined(__i386__)
+			printf(" %08X", *((long*)p + i));
+		#elif defined(__amd64__) || defined(__x86_64__)
+			printf(" %016lX", *((long*)p + i));
+		#endif
+		}
+		printf("\n");
+		p += -PLEN * PLEN;
+	}
+}
--- src/coroutine.h
+++ src/coroutine.h
@@ -1,7 +1,6 @@
 #ifndef _COROUTINE_H_
 #define _COROUTINE_H_
 
-#include <setjmp.h>
 #include <stdlib.h>
 
 #define RED_ZONE 128
@@ -9,39 +8,77 @@
 #define PAGE_SIZE 4096
 #define STK_DEFAULT_SIZE (64 * 1024)
 
+#if defined(__i386__)
+
+#define RLEN 6
+
+#elif defined(__amd64__) || defined(__x86_64__)
+
+#define RLEN 8
+
+#endif
+
 struct coroutine;
 
 typedef void (*coro_cb_t)(struct coroutine*);
 
+typedef enum cstat {
+	C_INIT,
+	C_PEND,
+	C_RUN,
+	C_END
+} cstat_t;
+
+typedef void* regbuf_t[RLEN];
+
 typedef struct coroutine {
-	jmp_buf env;
-	jmp_buf buf;
+	cstat_t state;
+	void* ctx;
+	regbuf_t env;
 	void* stk;
 	void* top;
 	void* bot;
 	void* sp;
 	coro_cb_t main;
 	void* res;
-	void* args;
-} *coroutine_t;
+} coroutine_t;
 
-coroutine_t coroutine_new(void);
+typedef coroutine_t* Coroutine;
+
+Coroutine coroutine_new(void);
 
 #define coroutine_free(coro) free((coro))
-//void coroutine_free(coroutine_t);
 
-void coroutine_init(coroutine_t, coro_cb_t);
+void coroutine_init(Coroutine, regbuf_t, coro_cb_t);
 
-extern int setreg(jmp_buf);
+extern int setreg(regbuf_t);
 
-extern void regjmp(jmp_buf, int);
+extern void regjmp(regbuf_t, int);
 
-#define coroutine_yield(coro)    \
-	if(setreg((coro)->buf) == 0) \
-		regjmp((coro)->env, 1)
+#define coroutine_yield(coro)      \
+	if(setreg((coro)->env) == 0) { \
+		(coro)->state = C_PEND;    \
+		regjmp((coro)->ctx, 1);   \
+	}
 
-#define coroutine_call(coro)      \
-	if(setreg((coro)->env) == 0) \
-		regjmp((coro)->buf, 1)
+#define coroutine_resume(coro)      \
+	if(setreg((coro)->ctx) == 0) { \
+		(coro)->state = C_RUN;      \
+		regjmp((coro)->env, 1);     \
+	}
+
+#define coroutine_reset(coro)       \
+	if(setreg((coro)->ctx) == 0) { \
+		regjmp((coro)->env, -1);    \
+	}
+
+#define coroutine_status(coro) (coro)->state
+
+/* For Debug */
+#if defined(DEBUG)
+void coroutine_dump_regs(Coroutine);
+
+void coroutine_dump_stack(Coroutine);
+#endif // DEBUG
 
 #endif // _COROUTINE_H_
