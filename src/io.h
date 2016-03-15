32399663e5ff8223f574c4d5ec7e618e50623be9
CodeSun <cscodesun@gmail.com>
2016-03-15 19:54:56 +0800

    [dev]add IO_timedReadSpec & IO_timedWriteSpec

--- src/io.h
+++ src/io.h
@@ -48,7 +48,8 @@ static inline ssize_t IO_readSpec(Handler h, void* buf, size_t len) {
 	}
 	return len;
 }
-static inline ssize_t IO_timedRead(Handler h, void* buf, size_t len, uint32_t to) {
+
+static inline int IO_waitEvent(Handler h, uint32_t to) {
 	struct timeval* timeout = NULL;
 	struct timeval tv;
 	if(to >= 0) {
@@ -62,34 +63,38 @@ static inline ssize_t IO_timedRead(Handler h, void* buf, size_t len, uint32_t to
 	fd_set set;
 	FD_ZERO(&set);
 	FD_SET(fd, &set);
-	int ret = select(fd + 1, &set, NULL, NULL, timeout);
-	switch(ret) {
+	return select(fd + 1, &set, NULL, NULL, timeout);
+}
+
+static inline ssize_t IO_timedRead(Handler h, void* buf, size_t len, uint32_t to) {
+	switch(IO_waitEvent(h, to)) {
 		case 0:  errno = ETIMEDOUT;
 		case -1: return -1;
 	}
 	return IO_read(h, buf, len);
-}
-
+} 
 static inline ssize_t IO_timedWrite(Handler h, void* buf, size_t len, uint32_t to) {
-	struct timeval* timeout = NULL;
-	struct timeval tv;
-	if(to >= 0) {
-		int sec  = to / 1000;
-		int usec = (to - sec) * 1000;
-		tv.tv_sec  = sec;
-		tv.tv_usec = usec;
-		timeout = &tv;
-	}
-	int fd = h->fileno;
-	fd_set set;
-	FD_ZERO(&set);
-	FD_SET(fd, &set);
-	int ret = select(fd + 1, NULL, &set, NULL, timeout);
-	switch(ret) {
+	switch(IO_waitEvent(h, to)) {
 		case 0:  errno = ETIMEDOUT;
 		case -1: return -1;
 	}
 	return IO_write(h, buf, len);
+}
+
+static inline ssize_t IO_timedReadSpec(Handler h, void* buf, size_t len, uint32_t to) {
+	switch(IO_waitEvent(h, to)) {
+		case 0:  errno = ETIMEDOUT;
+		case -1: return -1;
+	}
+	return IO_readSpec(h, buf, len);
+}
+
+static inline ssize_t IO_timedWriteSpec(Handler h, void* buf, size_t len, uint32_t to) {
+	switch(IO_waitEvent(h, to)) {
+		case 0:  errno = ETIMEDOUT;
+		case -1: return -1;
+	}
+	return IO_writeSpec(h, buf, len);
 }
 
 #endif //_IO_H_
