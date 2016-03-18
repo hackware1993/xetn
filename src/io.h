efe6ac58ad83dd63dffd22dbf414a7b6c990034f
CodeSun <cscodesun@gmail.com>
2016-03-18 09:59:24 +0800

    [dev]change the return value of io functions

--- src/io.h
+++ src/io.h
@@ -12,41 +12,63 @@
 #include <sys/select.h>
 #include <sys/types.h>
 
-#define IO_write(H, buf, len) write((H)->fileno, buf, len)
-#define IO_read(H, buf, len)  read((H)->fileno, buf, len)
-
-static inline ssize_t IO_writeSpec(Handler h, void* buf, size_t len) {
-	int n = 0;
-	size_t nleft = len;
-	while(nleft) {
-		n = IO_write(h, buf, nleft);
-		if(n == -1) {
-			if(errno == EAGAIN) {
-				return len - nleft;
-			}
-			return -1;
-		}
-		buf   += n;
-		nleft -= n;
+static inline int IO_write(Handler h, void* buf, size_t* len) {
+	int n = write(h->fileno, buf, *len);
+	switch(n) {
+		case -1: return -1;
+		default:
+			*len = n;
 	}
-	return len;
+	return 0;
 }
 
-static inline ssize_t IO_readSpec(Handler h, void* buf, size_t len) {
-	int n = 0;
-	size_t nleft = len;
-	while(nleft) {
-		n = IO_read(h, buf, nleft);
+static inline int IO_read(Handler h, void* buf, size_t* len) {
+	int n = read(h->fileno, buf, *len);
+	switch(n) {
+		/* END OF FILE */
+		case 0:  return 1;
+		case -1: return -1;
+		default:
+			*len = n;
+	}
+	return 0;
+}
+
+static inline int IO_writeSpec(Handler h, void* buf, size_t* len) {
+	int    n      = 0;
+	size_t total  = *len;
+	size_t nwrite = total;
+	while(total) {
+		n = IO_write(h, buf, &nwrite);
 		if(n == -1) {
-			if(errno == EAGAIN) {
-				return len - nleft;
-			}
+			*len = *len - total;
 			return -1;
 		}
-		buf   += n;
-		nleft -= n;
+		buf   += nwrite;
+		total -= nwrite;
+		nwrite = total;
 	}
-	return len;
+	return 0;
+}
+
+static inline int IO_readSpec(Handler h, void* buf, size_t* len) {
+	int    n     = 0;
+	size_t total = *len;
+	size_t nread = total;
+	while(total) {
+		n = IO_read(h, buf, &nread);
+		if(n == -1) {
+			*len = *len - total;
+			return -1;
+		} else if(n == 1) {
+			*len = *len - total;
+			return 1;
+		}
+		buf   += nread;
+		total -= nread;
+		nread  = total;
+	}
+	return 0;
 }
 
 static inline int IO_waitEvent(Handler h, int32_t to) {
@@ -66,14 +88,14 @@ static inline int IO_waitEvent(Handler h, int32_t to) {
 	return select(fd + 1, &set, NULL, NULL, timeout);
 }
 
-static inline ssize_t IO_timedRead(Handler h, void* buf, size_t len, int32_t to) {
+static inline ssize_t IO_timedRead(Handler h, void* buf, size_t* len, int32_t to) {
 	switch(IO_waitEvent(h, to)) {
 		case 0:  errno = ETIMEDOUT;
 		case -1: return -1;
 	}
 	return IO_read(h, buf, len);
 } 
-static inline ssize_t IO_timedWrite(Handler h, void* buf, size_t len, int32_t to) {
+static inline ssize_t IO_timedWrite(Handler h, void* buf, size_t* len, int32_t to) {
 	switch(IO_waitEvent(h, to)) {
 		case 0:  errno = ETIMEDOUT;
 		case -1: return -1;
@@ -81,7 +103,7 @@ static inline ssize_t IO_timedWrite(Handler h, void* buf, size_t len, int32_t to
 	return IO_write(h, buf, len);
 }
 
-static inline ssize_t IO_timedReadSpec(Handler h, void* buf, size_t len, int32_t to) {
+static inline ssize_t IO_timedReadSpec(Handler h, void* buf, size_t* len, int32_t to) {
 	switch(IO_waitEvent(h, to)) {
 		case 0:  errno = ETIMEDOUT;
 		case -1: return -1;
@@ -89,7 +111,7 @@ static inline ssize_t IO_timedReadSpec(Handler h, void* buf, size_t len, int32_t
 	return IO_readSpec(h, buf, len);
 }
 
-static inline ssize_t IO_timedWriteSpec(Handler h, void* buf, size_t len, int32_t to) {
+static inline ssize_t IO_timedWriteSpec(Handler h, void* buf, size_t* len, int32_t to) {
 	switch(IO_waitEvent(h, to)) {
 		case 0:  errno = ETIMEDOUT;
 		case -1: return -1;
