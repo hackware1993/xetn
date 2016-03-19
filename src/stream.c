fe9ab147738c51c6046ada8fa39befd559ec35f1
CodeSun <cscodesun@gmail.com>
2016-03-19 23:58:46 +0800

    [dev]rewrite stream module

--- src/stream.c
+++ src/stream.c
@@ -1,112 +1,69 @@
 #include "stream.h"
+#include "io.h"
 
-#include <stddef.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <errno.h>
-#include <sys/select.h>
-#include <sys/time.h>
-#include <errno.h>
+NetStream NetStream_init(NetStream ns, Handler h, uint32_t s) {
+	ns->handler = *h;
+	ns->buf = Buffer_new(s);
+	ns->errnum = 0;
+	return ns;
+}
 
-iostate_t Stream_timedRead(Handler handler, Buffer buf, int32_t to) {
-	struct timeval* timeout = NULL;
-	struct timeval tv;
-	if(to >= 0) {
-		int sec  = to / 1000;
-		int usec = (to - sec) * 1000;
-		tv.tv_sec  = sec;
-		tv.tv_usec = usec;
-		timeout = &tv;
+void NetStream_close(NetStream ns) {
+	// TODO consider if there should shutdown the sock
+	Buffer_free(ns->buf);
+}
+
+stream_state_t NetStream_readToBuf(NetStream ns) {
+	Buffer  buf   = ns->buf;
+	if(Buffer_isFull(buf)) {
+		return STREAM_DONE;
 	}
-	int fd = handler->fileno;
 	int ret;
-	fd_set set;
-	FD_ZERO(&set);
-	FD_SET(fd, &set);
-	ret = select(fd + 1, &set, NULL, NULL, timeout);
-	if(ret == 0) {
-		errno = ETIMEDOUT;
-		return S_ERR;
-	} else if(ret == -1) {
-		return S_ERR;
+	size_t len = buf->lim - buf->end;
+	/* DO NOT use IO_readSpec */
+	/* because IO can be blocked */
+	ret = IO_read(&ns->handler, buf->ptr + buf->end, &len);
+	buf->end += len;
+	switch(ret) {
+		case -1:
+			if(errno == EAGAIN) {
+				return STREAM_PEND;
+			}
+			ns->errnum = errno;
+			return STREAM_ERROR;
+		case 0:
+			return STREAM_DONE;
+		case 1:
+			return STREAM_HUP;
 	}
-	return Stream_read(handler, buf);
+
 }
 
-iostate_t Stream_timedWrite(Handler handler, Buffer buf, int32_t to) {
-	struct timeval* timeout = NULL;
-	struct timeval tv;
-	if(to >= 0) {
-		int sec  = to / 1000;
-		int usec = (to - sec) * 1000;
-		tv.tv_sec  = sec;
-		tv.tv_usec = usec;
-		timeout = &tv;
+stream_state_t NetStream_writeFromBuf(NetStream ns) {
+	Buffer  buf   = ns->buf;
+	if(Buffer_isEnd(buf)) {
+		return STREAM_DONE;
 	}
-	int fd = handler->fileno;
 	int ret;
-	fd_set set;
-	FD_ZERO(&set);
-	FD_SET(fd, &set);
-	ret = select(fd + 1, NULL, &set, NULL, timeout);
-	if(ret == 0) {
-		errno = ETIMEDOUT;
-		return S_ERR;
-	} else if(ret == -1) {
-		return S_ERR;
+	size_t len = buf->end - buf->pos;
+	/* DO NOT use IO_writeSpec */
+	/* because IO can be blocked */
+	ret = IO_write(&ns->handler, buf->ptr + buf->pos, &len);
+	buf->pos += len;
+	// TODO: check this
+	if(Buffer_isEnd(buf)) {
+		Buffer_setPos(buf, 0);
+		Buffer_setLen(buf, 0);
 	}
-	return Stream_write(handler, buf);
+	switch(ret) {
+		case -1:
+			if(errno = EAGAIN) {
+				return STREAM_PEND;
+			}
+			ns->errnum = errno;
+			return STREAM_ERROR;
+		case 0:
+			return STREAM_DONE;
+	}
 }
 
-iostate_t Stream_read(Handler handler, Buffer buf) {
-	fd_t fd = handler->fileno;
-	char* ptr = Buffer_getPtr(buf) + Buffer_getLen(buf);
-	size_t nleft = Buffer_getLim(buf) - Buffer_getLen(buf);
-	ssize_t nread = 0;
-	while(nleft > 0) {
-		if((nread = read(fd, ptr, nleft)) < 0) {
-			switch(errno) {
-				case EINTR:
-					nread = 0;
-					break;
-				case EAGAIN:
-					Buffer_setLen(buf, Buffer_getLim(buf) - nleft);
-					return S_PEND;
-				default:
-					return S_ERR;
-			}
-		} else if(nread == 0) {
-			Buffer_setLen(buf, Buffer_getLim(buf) - nleft);
-			return S_FIN;
-		}
-		ptr += nread;
-		nleft -= nread;
-	}
-	Buffer_setLen(buf, Buffer_getLim(buf));
-	return S_FULL;
-}
-
-iostate_t Stream_write(Handler handler, Buffer buf) {
-	fd_t fd = handler->fileno;
-	char* ptr = Buffer_getPtr(buf) + Buffer_getPos(buf);
-	size_t nleft = Buffer_getLen(buf) - Buffer_getPos(buf);
-	ssize_t nwritten = 0;
-	while(nleft > 0) {
-		if((nwritten = write(fd, ptr, nleft)) < 0) {
-			switch(errno) {
-				case EINTR:
-					nwritten = 0;
-					break;
-				case EAGAIN:
-					Buffer_setPos(buf, Buffer_getLen(buf) - nleft);
-					return S_PEND;
-				default:
-					return S_ERR;
-			}
-		}
-		ptr += nwritten;
-		nleft -= nwritten;
-	}
-	Buffer_setPos(buf, Buffer_getLen(buf));
-	return S_FIN;
-}
--- src/stream.h
+++ src/stream.h
@@ -4,18 +4,35 @@
 #include "handler.h"
 #include "buffer.h"
 
-typedef enum iostate {
-	S_NONE,
-	S_ERR,
-	S_PEND,
-	S_FULL,
-	S_FIN
-} iostate_t;
+typedef enum stream_state {
+	STREAM_NONE,
+	STREAM_ERROR,
+	STREAM_PEND,
+	STREAM_HUP,
+	STREAM_DONE,
+} stream_state_t;
 
-/* iostream for common handler */
-iostate_t Stream_read(Handler, Buffer);
-iostate_t Stream_write(Handler, Buffer);
-iostate_t Stream_timedRead(Handler, Buffer, int32_t);
-iostate_t Stream_timedWrite(Handler, Buffer, int32_t);
+typedef enum stream_type {
+	INPUT_STREAM,
+	OUTPUT_STREAM,
+} stream_type_t;
+
+typedef struct net_stream {
+	handler_t handler;
+	Buffer    buf;
+	int       errnum;
+} net_stream_t, *NetStream;
+
+NetStream NetStream_init(NetStream, Handler, uint32_t);
+void      NetStream_close(NetStream);
+
+stream_state_t NetStream_read(NetStream, void*, size_t*);
+stream_state_t NetStream_write(NetStream, void*, size_t*);
+stream_state_t NetStream_flush(NetStream);
+
+/* WARNING: following functions are used internally */
+#define        NetStream_getBuf(ns) (ns)->buf
+stream_state_t NetStream_readToBuf(NetStream);
+stream_state_t NetStream_writeFromBuf(NetStream);
 
 #endif // _STREAM_H_
