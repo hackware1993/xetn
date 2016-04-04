b3e552259745d42655124311cdc1a5ad7c65f139
CodeSun <cscodesun@gmail.com>
2016-04-04 15:56:22 +0800

    [misc]separate .c & .h files

---
+++ include/common.h
@@ -0,0 +1,11 @@
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+#include <stdint.h>
+#include "optimize.h"
+
+#define PRIVATE static
+#define INLINE __inline__ __attribute__((always_inline))
+
+
+#endif //_COMMON_H_
