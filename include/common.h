cc9c885ce0270cf114b21ff0f1473fc1d69ae742
CodeSun <cscodesun@gmail.com>
2016-04-04 15:59:23 +0800

    [misc]combine common.h & optimize.h

--- include/common.h
+++ include/common.h
@@ -2,10 +2,17 @@
 #define _COMMON_H_
 
 #include <stdint.h>
-#include "optimize.h"
 
 #define PRIVATE static
 #define INLINE __inline__ __attribute__((always_inline))
+
+#if __GNUC__ >= 3
+	#define LIKELY(x) __builtin_expect(!!(x), 1)
+	#define UNLIKELY(x) __builtin_expect(!!(x), 0)
+#else
+	#define LIKELY(x) (x)
+	#define UNLIKELY(x) (x)
+#endif
 
 
 #endif //_COMMON_H_
